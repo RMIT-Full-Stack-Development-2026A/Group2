083936bb4ba7dbebadc3250c10f3b38eeaf0b567
pxhung2511 <154577003+pxhung2511@users.noreply.github.com>
2026-04-04 15:31:01 +0700

    add route login and debug

--- backend/src/modules/auth/auth.route.js
+++ backend/src/modules/auth/auth.route.js
@@ -4,5 +4,7 @@ const authController = require("./auth.controller");
 const router = express.Router();
 
 router.post("/register", authController.signUp);
+router.post("/login", authController.logIn);
+router.post("/logout", authController.logOut);
 
 module.exports = router;
