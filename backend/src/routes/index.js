5a988395b53564ae71e0aa1aa7e9987d979cdec9
empty06 <s4022154@rmit.edu.vn>
2026-04-16 01:13:06 +0700

    Revert "resolve merge conflicts for profile and game setup paths"
    This reverts commit b3dc412f2328c6301496bcd381693300da22d30e, reversing
    changes made to 4e3b8a125f5b82e4b28f488513dde60b4901934c.

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,5 +1,3 @@
 const authRouter = require("../modules/auth/auth.route");
 const gameRouter = require("../modules/game/api/routes/game.route");
-const profileRouter = require("../modules/profile/profile.route");
-
-module.exports = { authRouter, gameRouter, profileRouter };
+module.exports = { authRouter, gameRouter };
